aa0fd5bb1cc36d7a22ee708793676cd4b1d3bd27
Brian Mullin <brmullin@email.msmary.edu>
2022-11-16 22:52:04 -0500

    add logic to merkle tree test

--- test/merkle-root.js
+++ test/merkle-root.js
@@ -13,6 +13,44 @@ function encodeLeaf(address, spots) {
 
 describe("Check if merkle root is working", function() {
     it("Should be able to verify if a given address is in whitelist or not", async function () {
-        
+
+        // Get a bunch of test addresses
+        const [owner, addr1, addr2, addr3, addr4, addr5] = 
+            await ethers.getSigner();
+
+        // Create an array of elements you wish to encode in the Merkle Tree
+        const list = [
+            encodeLeaf(owner.address, 2),
+            encodeLeaf(addr1.address, 2),
+            encodeLeaf(addr2.address, 2),
+            encodeLeaf(addr3.address, 2),
+            encodeLeaf(addr4.address, 2),
+            encodeLeaf(addr5.address, 2),
+        ];
+
+        // Create the Merkle Tree using the hashing algorithm `kecck256`
+        // Make sure to sort the tree so that it can be produced deterministically regardless of the order of the input list
+        const merkleTree = new MerkleTree(list, keccak256, {
+            hashLeaves: true,
+            sortPairs: true,
+        });
+
+        // Compute the Merkle Root
+        const root = merkleTree.getHexRoot();
+
+        // Deploy the Whitelist contract
+        const whitelist = await ethers.getContractFactory("Whitelist");
+        const Whitelist = await whitelist.deploy(root);
+        await Whitelist.deployed();
+
+        // Compute the Merkle Proof of the owner address (0'th item in list) off-chain.
+        // The leaf node is the hash of that value.
+        const leaf = keccak256(list[0]);
+        const proof = merkleTree.getHexProof(leaf);
+
+        // Provide the Merkle Proof to the contract, and ensure that it can verify
+        // that this leaf node was indeed part of the Merkle Tree 
+        let verified = await Whitelist.checkInWhiteList([], 2);
+        expect(verified).to.equal(false);
     });
 });
